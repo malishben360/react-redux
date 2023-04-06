ab8885d09cef86ae9978d3848f8fa5519b527d7b
Elisha Benjamin <malishben360@gmail.com>
2023-04-06 13:36:02 +0100

    Feature: HookContainer view for reat-redux hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CakeContainer from './components/CakeContainer';
 import './App.css';
+import HookContainer from './components/HookContainer';
 
 function App() {
   return (
     <div className="App">
+      <HookContainer />
       <CakeContainer />
     </div>
   );
---
+++ src/components/HookContainer.js
@@ -0,0 +1,10 @@
+function HookContainer() {
+    return ( 
+        <div>
+            <h2>Num of Cakes - 10</h2>
+            <button>Buy Cake</button>
+        </div>
+     );
+}
+
+export default HookContainer;
