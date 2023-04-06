ab905207317032c9d3ffe07f131d574a82ca149b
Elisha Benjamin <malishben360@gmail.com>
2023-04-06 14:27:54 +0100

    Update: use the redux state and cakeReducer using react-redux hooks

--- src/components/HookContainer.js
+++ src/components/HookContainer.js
@@ -1,8 +1,13 @@
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../redux";
+
 function HookContainer() {
+    const numOfCakes = useSelector((state) => state.numOfCakes);
+    const dispatch = useDispatch();
     return ( 
         <div>
-            <h2>Num of Cakes - 10</h2>
-            <button>Buy Cake</button>
+            <h2>Num of Cakes - { numOfCakes }</h2>
+            <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
         </div>
      );
 }
