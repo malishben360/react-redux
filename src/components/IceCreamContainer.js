394d9439e9d6030176bf03872502220a7c1aea87
Elisha Benjamin <malishben360@gmail.com>
2023-04-06 14:35:54 +0100

    Feature: icecream view

---
+++ src/components/IceCreamContainer.js
@@ -0,0 +1,10 @@
+function IceCreamContainer() {
+    return ( 
+        <div>
+            <h2>Number of Icecreams - 15</h2>
+            <button>Buy Icecream</button>
+        </div>
+     );
+}
+
+export default IceCreamContainer;
