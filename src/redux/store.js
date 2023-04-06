e31cdda419378ad9af87982d41ed4fd4adaff0d6
Elisha Benjamin <malishben360@gmail.com>
2023-04-06 16:00:23 +0100

    Feature: logger middleware to logging state transitions on browser

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { legacy_createStore as createStore} from "redux";
+import { legacy_createStore as createStore, applyMiddleware} from "redux";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+
 
 /** Currently there is new better version(@reduxjs/toolkit) of redux for creating the redux store.
  * The createStore function has been deprecated, but legacy_createStore function is made availlable.
  */
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
